bdff7d223a820c91cb99652e7c252ce8637b492d
OleksandrGavrylenko <gavrylenko.aleksandr@gmail.com>
2013-12-05 08:40:36 +0200

    First commit

---
+++ Spring Project/src/main/java/modules/CalculateScreen.java
@@ -0,0 +1,18 @@
+package modules;
+
+public class CalculateScreen {
+	private Operation ops = new OpMultiply();
+	private ResultWriter wtr = new ScreenWriter();
+
+	public static void main(String[] args) {
+		CalculateScreen calc = new CalculateScreen();
+		calc.execute(args);
+	}
+
+	public void execute(String [] args) {
+		long op1 = Long.parseLong(args[0]);
+
+		long op2 = Long.parseLong(args[1]);
+		wtr.showResult("The result of " + op1 + ops.getOpsName() + op2 + " is " + ops.operate(op1, op2) + "!");
+	}
+}
